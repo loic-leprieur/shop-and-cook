8acdb6479648bae4100e26e76ed29c11603aeebf
leprieur <donleprieur@outlook.fr>
2016-11-22 20:18:44 +0100

    Comments on futures modifications and tweaks

--- app/src/main/java/be/vives/loic/shopandcook/MainActivity.java
+++ app/src/main/java/be/vives/loic/shopandcook/MainActivity.java
@@ -7,8 +7,13 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+// TODO : Switch to a Fragment layout design
 public class MainActivity extends AppCompatActivity {
     ListView list;
+
+    // temporary list, will be replaced by a DB of 10 recipe
+    // those 10 recipes will be randomly choose and
+    // deleted changed at each refreshment of the activity
     String[] web = {
             "Google Plus",
             "Twitter",
@@ -18,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
             "Wordpress",
             "Drupal"
     } ;
+
+    // pictures will be temporarly stored or accessed
+    // by their URL and displayed in the list
     Integer[] imageId = {
             R.drawable.image1,
             R.drawable.image2,
@@ -29,22 +37,35 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+    /**
+     * At activity launch, create a list of items,
+     * define the XML layout to use and link
+     * the event listeners with the list.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CustomList adapter = new
-                CustomList(MainActivity.this, web, imageId);
+        CustomList adapter = new CustomList(MainActivity.this, web, imageId);
+
         list=(ListView)findViewById(R.id.list);
         list.setAdapter(adapter);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+            /**
+             * Click on item handler,
+             * will be replaced by an inflated list on trigger.
+             * @param parent
+             * @param view
+             * @param position
+             * @param id
+             */
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 Toast.makeText(MainActivity.this, "You Clicked at " +web[+ position], Toast.LENGTH_SHORT).show();
-
             }
         });
     }
