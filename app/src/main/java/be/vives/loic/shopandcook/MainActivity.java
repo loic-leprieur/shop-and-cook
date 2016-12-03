d6e282117f8eb5f2b035c8b1df6909d09319e34e
leprieur <donleprieur@outlook.fr>
2016-12-03 14:13:44 +0100

    Cleaning code, remove duplicate code for sign in (new activity already deals with it)

--- app/src/main/java/be/vives/loic/shopandcook/MainActivity.java
+++ app/src/main/java/be/vives/loic/shopandcook/MainActivity.java
@@ -34,9 +34,7 @@ import com.google.firebase.database.FirebaseDatabase;
 // TODO : Switch to a Fragment layout design
 // TODO : From in-memory database to Firebase DB
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-    private static final int RC_SIGN_IN = 200; // 200 = successfully signed in
-
+    // List view variables
     ListView list;
 
     // temporary list, will be replaced by a DB of 10 recipe
@@ -64,13 +62,8 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.image7
     };
 
-    // Firebase fiels for 'AUTHENTIFICATION'
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseUser mFirebaseUser;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private DatabaseReference mDatabase;
-    private String mUserId;
-    private GoogleApiClient mGoogleApiClient;
+    // Firebase database fields
+    private FirebaseDatabase mFirebaseDB;
 
     /**
      * At activity launch, create a list of items,
@@ -85,82 +78,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Instance of a connection to Firebase
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-
-        if(mFirebaseUser == null){
-            // not logged in, launch the log in activity
-            loadLogInView();
-        }else {
-            mUserId = mFirebaseUser.getUid();
-
-            // Set up ListView
-            final ListView listView = (ListView) findViewById(R.id.list);
-            final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
-            listView.setAdapter(adapter);
-
-            // Add items via the Button and EditText at the bottom of the view
-            final EditText text = (EditText) findViewById(R.id.textSearch);
-            final Button button = (Button) findViewById(R.id.buttonSearch);
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mDatabase.child("users")
-                            .child(mUserId)
-                            .child("websites")
-                            .push()
-                            .child("title")
-                            .setValue(text.getText().toString());
-                    text.setText("");
-                }
-            });
-
-            // Use Firebase to populate the list
-            mDatabase.child("users")
-                    .child(mUserId)
-                    .addChildEventListener(new ChildEventListener() {
-                        @Override
-                        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                            adapter.add((String) dataSnapshot.child("title").getValue());
-                        }
-
-                        @Override
-                        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-                        }
-
-                        @Override
-                        public void onChildRemoved(DataSnapshot dataSnapshot) {
-                            adapter.remove((String) dataSnapshot.child("title").getValue());
-                        }
-
-                        @Override
-                        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
-                        }
-                    });
-
-        }
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                }
-            }
-        };
+        mFirebaseDB = FirebaseDatabase.getInstance();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
         // set up the ListView
         list = (ListView) findViewById(R.id.list);
@@ -184,69 +103,5 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "You Clicked at " + web[+position], Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    private void loadLogInView() {
-    }
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = result.getSignInAccount();
-                firebaseAuthWithGoogle(account);
-            } else {
-                // Google Sign In failed, update UI appropriately
-                Toast.makeText(MainActivity.this, "Google sign in failed", Toast.LENGTH_LONG);
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mFirebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithCredential", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                        // ...
-                    }
-                });
-    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mFirebaseAuth.addAuthStateListener(mAuthListener);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if(mAuthListener != null){
-            mFirebaseAuth.removeAuthStateListener(mAuthListener);
-        }
     }
 }
