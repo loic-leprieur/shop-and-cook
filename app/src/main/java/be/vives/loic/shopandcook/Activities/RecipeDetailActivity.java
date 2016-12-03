419f52345b23c7411f32eb926a7f3d00d961ce50
leprieur <donleprieur@outlook.fr>
2016-12-03 23:34:09 +0100

    onCreate use the new recipe title to update the detail fragment (textview: recipe_title)

--- app/src/main/java/be/vives/loic/shopandcook/Activities/RecipeDetailActivity.java
+++ app/src/main/java/be/vives/loic/shopandcook/Activities/RecipeDetailActivity.java
@@ -2,6 +2,7 @@ package be.vives.loic.shopandcook.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import be.vives.loic.shopandcook.R;
 
@@ -12,6 +13,11 @@ public class RecipeDetailActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+        setContentView(R.layout.activity_details2);
+
+        String recipe_title = getIntent().getExtras().getString("selectedRecipe");
+
+        TextView title = (TextView) findViewById(R.id.recipe_title);
+        title.setText(recipe_title);
     }
 }
