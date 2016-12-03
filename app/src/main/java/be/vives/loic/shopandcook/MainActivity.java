cc28905efa845fb860de0110c212fe2e07968791
leprieur <donleprieur@outlook.fr>
2016-12-03 16:18:21 +0100

    Creation of the UI for Details and List Fragment layouts

--- app/src/main/java/be/vives/loic/shopandcook/MainActivity.java
+++ app/src/main/java/be/vives/loic/shopandcook/MainActivity.java
@@ -1,37 +1,16 @@
 package be.vives.loic.shopandcook;
 
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-// TODO : Switch to a Fragment layout design
 // TODO : From in-memory database to Firebase DB
 public class MainActivity extends AppCompatActivity {
     // List view variables
@@ -40,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     // temporary list, will be replaced by a DB of 10 recipe
     // those 10 recipes will be randomly choose and
     // deleted changed at each refreshment of the activity
-    String[] web = {
+    String[] recipes_title = {
             "Fondants au chocolat",
             "Tarte au citron meringuée",
             "Filet mignon en croûte",
@@ -83,10 +62,10 @@ public class MainActivity extends AppCompatActivity {
 
         // set up the ListView
         list = (ListView) findViewById(R.id.list);
-        final RecipeArrayAdapter adapter = new RecipeArrayAdapter(MainActivity.this, web, imageId);
+        final RecipeArrayAdapter adapter = new RecipeArrayAdapter(MainActivity.this, recipes_title, imageId);
         list.setAdapter(adapter);
 
-        // Show the position and text from an item clicked
+        // on click event handler : change to a new fragment details here
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             /**
@@ -100,7 +79,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-                Toast.makeText(MainActivity.this, "You Clicked at " + web[+position], Toast.LENGTH_SHORT).show();
+                // Show the position and text from an item clicked
+                Toast.makeText(MainActivity.this, recipes_title[+position], Toast.LENGTH_SHORT).show();
             }
         });
     }
