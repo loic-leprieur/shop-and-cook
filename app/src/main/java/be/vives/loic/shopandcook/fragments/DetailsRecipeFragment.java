def13601dd88d20b7059046da8cc8170f8b9c34a
leprieur <donleprieur@outlook.fr>
2017-01-11 20:43:20 +0100

    Cleanup imports

--- app/src/main/java/be/vives/loic/shopandcook/fragments/DetailsRecipeFragment.java
+++ app/src/main/java/be/vives/loic/shopandcook/fragments/DetailsRecipeFragment.java
@@ -6,13 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import be.vives.loic.shopandcook.R;
 import be.vives.loic.shopandcook.models.Recipe;
-import be.vives.loic.shopandcook.models.RecipeRepository;
 
 /**
  * Created by LOIC on 03/12/2016.
@@ -24,7 +19,6 @@ public class DetailsRecipeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Nullable
